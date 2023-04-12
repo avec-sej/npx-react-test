513ead1f5c47693724b60cbf34b675cd3d956429
avec-sej <sug7761@gmail.com>
2023-04-12 21:41:30 +0900

    영화웹페이지

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () =>  setValue(prev => prev + 1);
+
+  console.log("I run all the time");
+  const runOnlyOnce = () => {
+    console.log("I run only once");
+  };
+  useEffect(runOnlyOnce, []);
+
   return (
     <div>
       <h1 className={styles.title}>Welcome back!!!</h1>
