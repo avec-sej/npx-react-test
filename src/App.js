a15ff7a1a7b73dc912d2c23fdb98f8bbcfee5a2d
avec-sej <sug7761@gmail.com>
2023-04-26 23:33:50 +0900

    npx

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const[toDo, setTodo] = useState("");  
-  const[toDos, setTodos] = useState([]);
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo == ""){
-      return;
-    }
-    setTodos(currentArray => [toDo, ...currentArray]);
-    setTodo("");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [amount, setAmount] = useState(0);
+  const [index, setIndex] = useState();
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then(response => response.json())
+      .then(json => {
+        setCoins(json); 
+        setLoading(false);
+      });
+  },[]);
+  const getAmount = (event) => {
+    setAmount(event.target.value);
   };
-  
+  const onSelect = (event) => {
+    setIndex(event.target.value);
+    console.log(event.target.value);
+  };
   return (
     <div>
-      <h1>My To Do List({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          value={toDo} 
-          onChange={onChange} 
-          type="text" 
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map(
-          (item, index) => (
-            <li key={index }>{item}</li>
+      <h1>The Coins! {loading? "":`(How many? ${coins.length})`}</h1>
+      {loading? <strong>Loading...</strong>:
+      <select value={index} onSelect={onSelect}>
+        {coins.map((coin) => (
+          <option key={coin.id}>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD 
+          </option>
         ))}
-      </ul>
-      
+      </select>
+      }
+      <hr/>
+      <label>I have $ </label>
+      <input 
+        type="number" 
+        value={amount} 
+        onChange={getAmount}
+      />
+      <p>You can buy {index}</p>
     </div>
   )
 }
---
+++ src/App3.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const[toDo, setTodo] = useState("");  
+  const[toDos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo == ""){
+      return;
+    }
+    setTodos(currentArray => [toDo, ...currentArray]);
+    setTodo("");
+  };
+  
+  return (
+    <div>
+      <h1>My To Do List({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          value={toDo} 
+          onChange={onChange} 
+          type="text" 
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map(
+          (item, index) => (
+            <li key={index }>{item}</li>
+        ))}
+      </ul>
+      
+    </div>
+  )
+}
+
+//export default App;
